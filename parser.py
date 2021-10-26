2a6d0cb19373d42b99c2b203327f1441874d3a94
Nichollette <nichollette.aco@gmail.com>
2021-10-26 12:49:07 -0400

    removed variable used in testing

--- parser.py
+++ parser.py
@@ -38,7 +38,7 @@ def get_gene(gene_id):
 
 
 # main method 
-def load_orthology(data, data_list):
+def load_orthology(data):
     # gather data file 
     infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
     assert os.path.exists(infile)
