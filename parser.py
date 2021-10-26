3a50b20398762b40b8b0653906d2e19e37cdeeb2
Nichollette <nichollette.aco@gmail.com>
2021-10-26 16:02:44 -0400

    testing on biothings

--- parser.py
+++ parser.py
@@ -1,13 +1,12 @@
 
 import os, pandas
 import numpy as np
-from biothings.utils.dataload import dict_convert, dict_sweep
-from biothings import config
+
 from biothings_client import get_client
+from biothings.utils.dataload import dict_convert, dict_sweep
+#from biothings import config
 
-logging = config.logger
-
-process_key = lambda k: k.replace(" ","_").lower()
+#logging = config.logger
 
 def setup_release(self):
     release="2019-06-23"
@@ -42,13 +41,14 @@ def get_gene(gene_id, gene_client):
 # main method 
 def load_orthology(data_folder):
     # setup data from the file
-    infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
+    infile = os.path.join(data_folder)#, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
     assert os.path.exists(infile)
 
     # use pandas to load -- update to use built-in package from utils !!!!!
-    data_ortho=pandas.read_csv(infile, header=15, sep="\\t").to_dict(orient='records')
+    data_ortho=pandas.read_csv(infile, header=15, sep="\\t", engine='python').to_dict(orient='records')
 
     results = {} # initialize final result dict 
+    final_list=[] # initialize final data list
     bad_queries=[] # initialize gene query ids that return None (empty)
 
     process_key = lambda k: k.replace(" ","_").lower() 
@@ -56,6 +56,7 @@ def load_orthology(data_folder):
     # initialize mygene object
     gene_client = get_client('gene') 
     
+
     # iterate over the data
     for rec in data_ortho:
 
@@ -84,5 +85,8 @@ def load_orthology(data_folder):
     # iterate through result items
     for _id,docs in results.items():
         doc = {"_id": _id, "agr": {"ortholog" : docs}}
+        final_list.append(doc)
         #print(json.dumps(doc, sort_keys=False, indent=4))
         yield doc
+    
+    #return final_list;
