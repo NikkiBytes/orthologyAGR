7d5aa34d2f8f3556f7cf80c8ce70424d286ca9c1
Nichollette <nichollette.aco@gmail.com>
2021-10-26 11:21:03 -0400

    update to parser and readme
    updated data

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Orthology AGR Data Plugin for BioThings 
+# Orthology AGR Data Plugin for BioThings   
+  
+<u> ## Document Structure </u>
+      
+`
+"_id": "WBGene00011502" --> "176377" by querying mygene.info API,
+  "agr": 
+     "ortholog": [
+        {
+          "geneid": "SGD:S000003566",
+          "symbol": "VPS53",
+          "taxid": "NCBITaxon:559292" --> 559292,
+          "algorithms": "PhylomeDB|OrthoFinder|Hieranoid|OMA|Ensembl Compara|Roundup|InParanoid|PANTHER|OrthoInspector",
+          "algorithmsmatch": 9,
+          "outofalgorithms": 10,
+          "isbestscore": "Yes" --> True,
+          "isbestrevscore": "Yes" --> True
+        }`  
+           
+
+<br>  
+<br>  
+
+## Notes  
+
+<br>
+
+IDs that returned None from `mygene.info` 
--- parser.py
+++ parser.py
@@ -11,24 +11,69 @@ def setup_release(self):
     release="2019-06-23"
     return release
 
+# boolean conversion method
+def convert_score(score):
+    if score=="Yes":
+        return True;
+    else:
+        return False;
 
-def load_orthology(data_folder):
+# document setup method
+def set_document(rec):
+    doc={
+        "geneid": rec['gene2id'],
+        "symbol": rec['gene2symbol'],
+        "taxid": rec['gene2speciestaxonid'], 
+        "algorithmsmatch": rec["algorithmsmatch"],
+        "outofalgorithms": rec["outofalgorithms"] ,
+        "isbestscore": convert_score(rec['isbestscore']),
+        "isbestrevscore": convert_score(rec['isbestrevscore'])
+    }
+    return doc;
 
+# gene query method
+def get_gene(gene_id):
+    gene=gene_client.getgene(gene_id, fields='symbol,name')
+    return gene;
+
+
+# main method 
+def load_orthology(data, data_list):
+    # gather data file 
     infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
     assert os.path.exists(infile)
-
     data_ortho=pandas.read_csv(infile, header=15, sep="\\t").to_dict(orient='records')
-    results = {}
+    results = {} # initialize final result dict 
+    bad_queries=[] # initialize gene query ids that return None (empty)
+    process_key = lambda k: k.replace(" ","_").lower()
 
+    # iterate over the data
     for rec in data_ortho:
+
+        # get the main ID and reformat 
         orig_id1= rec["Gene1ID"].split(':')
         id1_tag2=orig_id1[1]
         _id = id1_tag2
+
+        # query for the corresponding numeric id of the original id
+        gene=get_gene(_id)
+        
+        # check if gene id was not found
+        if not gene:
+            bad_queries.append(_id) # add no matching id
+        else:
+            _id = gene["_id"] # assign new id queried from mygene
+
         rec = dict_convert(rec,keyfn=process_key)
         # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
-        results.setdefault(_id,[]).append(rec)
 
+        # setup document
+        doc=set_document(rec)
+        results.setdefault(_id,[]).append(doc)
+
+    # iterate through result items
     for _id,docs in results.items():
-        doc = {"_id": _id, "ortholog_info" : docs}
+        doc = {"_id": _id, "agr": {"ortholog" : docs}}
+        #print(json.dumps(doc, sort_keys=False, indent=4))
         yield doc
