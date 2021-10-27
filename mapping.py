b43887d69cec2b2b4697c19629cf1268c469d1cc
Nichollette <nichollette.aco@gmail.com>
2021-10-27 13:47:28 -0400

    adding mapping file

---
+++ mapping.py
@@ -0,0 +1,35 @@
+def get_customizd_mapping(cls):
+    mapping={
+            "agr": {
+                "properties": {
+                    "ortholog": {
+                        "properties": {
+                            "geneid": {
+                                "normalizer": "keyword_lowercase_normalizer",
+                                "type": "keyword"
+                            },
+                            "symbol": {
+                                "normalizer": "keyword_lowercase_normalizer",
+                                "type": "keyword"
+                            },
+                            "taxid": {
+                                "type": "integer"
+                            },
+                            "algorithmsmatch": {
+                                "type": "integer"
+                            },
+                            "outofalgorithms": {
+                                "type": "integer"
+                            },
+                            "isbestscore": {
+                                "type": "boolean"
+                            },
+                            "isbestrevscore": {
+                                "type": "boolean"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    return mapping;
