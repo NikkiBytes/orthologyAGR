a14c36674099c4a3e73ad56fc201ac98c1094179
Nichollette <nichollette.aco@gmail.com>
2021-11-10 11:20:10 -0500

    adding updates from prior days, had issue with git push

--- README.md
+++ README.md
@@ -4,22 +4,70 @@
       
 ```
 
-  {
-  '_id': '176377',
-  'agr':
+[
     {
-    'ortholog': 
-      {
-        'geneid': 'SGD:S000003566',
-        'symbol': 'VPS53',
-        'taxid': 559292,
-        'algorithmsmatch': 9,
-        'outofalgorithms': 10,
-        'isbestscore': True,
-        'isbestrevscore': True
-      }
-    }
-  }
+        "_id": "176377",
+        "agr": {
+            "orthologs": [
+                {
+                    "geneid": "S000003566",
+                    "symbol": "VPS53",
+                    "taxid": 559292,
+                    "algorithmsmatch": 9,
+                    "outofalgorithms": 10,
+                    "isbestscore": true,
+                    "isbestrevscore": true
+                },
+                {
+                    "geneid": "33640",
+                    "symbol": "Vps53",
+                    "taxid": 7227,
+                    "algorithmsmatch": 9,
+                    "outofalgorithms": 10,
+                    "isbestscore": true,
+                    "isbestrevscore": true
+                },   
+                .  
+                .  
+                .  
+                .    
+                ]  
+        }
+    },
+    {
+        "_id": "492817",
+        "agr": {
+            "orthologs": [
+                {
+                    "geneid": "S000003566",
+                    "symbol": "VPS53",
+                    "taxid": 559292,
+                    "algorithmsmatch": 8,
+                    "outofalgorithms": 10,
+                    "isbestscore": true,
+                    "isbestrevscore": true
+                },
+                {
+                    "geneid": "33640",
+                    "symbol": "Vps53",
+                    "taxid": 7227,
+                    "algorithmsmatch": 9,
+                    "outofalgorithms": 11,
+                    "isbestscore": true,
+                    "isbestrevscore": true
+                },    
+                .  
+                .  
+                .  
+                .  
+                ]
+        }
+    },
+    .  
+    .  
+    .  
+    .   
+]        
 
 ```  
 
@@ -29,23 +77,29 @@
 <br>  
 
 ## Notes    
-- The variable, `"algorithms": "PhylomeDB|OrthoFinder|Hieranoid|OMA|Ensembl Compara|Roundup|InParanoid|PANTHER|OrthoInspector"` is available in the data file. Currently the created output document doesn't include it, but can easily be added. Note, if added, we considered reformatting the data string into a list.
+
+- **Entity-Centric** document structure.  
+<br>  
+
+- **Asthetic("display") detail":** The variable, `"algorithms": "PhylomeDB|OrthoFinder|Hieranoid|OMA|Ensembl Compara|Roundup|InParanoid|PANTHER|OrthoInspector"` is available in the data file. Currently the created output document doesn't include it, but can easily be added. Note, if added, we considered reformatting the data string into a list.
 
 <br>
 
-- translating the IDs to ncbi:gene id numeric: some data ids aren't found using the mygene.info client search.  
-    A list has been created to track what ids are not being found. We see there is a pattern: <br>  
+- Need to translate to the NCBI gene ID on some of the given gene IDs. Some of the IDs aren't found using the mygene.info client search.  
+    *A list is created to track what ids are not being found,*.  <br>   
+    - The `'ZDB-GENE-*` IDs translated with reference from the [ZFIN Database](https://zfin.org/downloads).  
+              File downloaded [ZFIN Marker associations to NCBI Gene data file]().  
+      <br>
 
-     ``` ['ZDB-GENE-041114-199', '1311391', '1915549', 'ZDB-GENE-070112-1002', '1586427', '2444430', 'ZDB-GENE-030131-260', 'ZDB-GENE-060929-828', 'ZDB-GENE-040426-2217', '621079', '620245', '2142527', '1566418', '1920462', 'ZDB-GENE-070209-131', 'ZDB-GENE-040426-1192', 'ZDB-GENE-050522-314', '1560053', '1563868', '2441730', '1336172', 'ZDB-GENE-040426-1869', 'ZDB-GENE-060616-266', 'ZDB-GENE-040718-60', 'ZDB-GENE-070424-85', '1589953', '1308104', '1303247', '2144766', '2145373', '2444911', '2147731', 'ZDB-GENE-030131-1394', 'ZDB-GENE-050809-111', '1306146', '2155936', 'ZDB-GENE-040122-2', 'ZDB-GENE-090313-225', 'ZDB-GENE-000831-3', '1195264', 'ZDB-GENE-050208-284', '1588894', '1914021', 'ZDB-GENE-061220-11', '1563497', '1351630', 'ZDB-GENE-030131-2768', 'ZDB-GENE-050522-302', '1310783', '1309128'] ```
+    - The `'SGD:S0*` IDs are the [SGD database]()
+<br>  
 
-  
-
-     For the `'ZDB-GENE-*` genes, the ZFIN database references the ID we need. Looking for a file that references the ID, _webscraping is an option too...?_  
-
-     **Need to determine the numeric IDs**  
+  **Need to determine what the numeric IDs are**  
 
 <br>  
 
-- verifying that `gene1id` and `gene2id` both reference eachother to confirm data document setup. 
+- Confirmed the combinations of the relations. The **pairwise links** between the IDs was searched between `gene1ID` and `gene2ID` to confirm the ID pairwise exists
+among groupings. Created document structure accordingly.  
+
   
 <br>
--- parser.py
+++ parser.py
@@ -1,7 +1,21 @@
+<<<<<<< HEAD
 
 import os
 import numpy as np
 import pandas as pd
+=======
+"""
+# Orthology AGR Data Parser  
+# Author: Nichollette T. Acosta  
+# Organization: Su and Wu labs @ Scripps Research  
+# Data parser for the Alliance of Genome Resources database for orthology relationships. 
+"""
+# load packages
+import os
+import pandas as pd
+#import numpy as np
+
+>>>>>>> 1b90fbc (adding updates from last few days)
 from biothings_client import get_client
 from biothings.utils.dataload import dict_convert, dict_sweep
 from biothings import config
@@ -33,7 +47,13 @@ def set_document(rec):
 
 
 # gene query method
+<<<<<<< HEAD
 def get_gene(gene_id, gene_client):
+=======
+def get_gene(gene_id):#, gene_client):
+    # initialize mygene object
+    gene_client = get_client('gene')
+>>>>>>> 1b90fbc (adding updates from last few days)
     gene=gene_client.getgene(gene_id, fields='symbol,name')
     return gene;
 
@@ -48,6 +68,7 @@ def orthology_search(gene_id, df):
 def load_orthology(data_folder):
     # setup data from the file
     infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED_51.tsv")
+<<<<<<< HEAD
     assert os.path.exists(infile)
 
     # use pandas to load -- update to use built-in package from utils !!!!!
@@ -56,12 +77,23 @@ def load_orthology(data_folder):
     #data_ortho=data_ortho.sort_values(by=['Gene1ID']) 
     #display(data_ortho[:5])
     
+=======
+    zfin_ncbi_file=os.path.join(data_folder, "gene_2021.11.02.txt")
+
+    assert os.path.exists(infile)
+
+    # use pandas to load -- update to use built-in package from utils !!!!!
+    data_ortho=pd.read_csv(infile, header=15, sep="\\t", engine='python')#.sort_values(by=['Gene1ID']) 
+    zfin_df=pd.read_csv(zfin_ncbi_file, sep="\\t", header=1, engine='python')
+
+>>>>>>> 1b90fbc (adding updates from last few days)
     # get unique value of ids from Gene1ID column
     unique_ids=data_ortho["Gene1ID"].unique()
 
     final_list=[] # initialize final data list
     bad_queries=[] # initialize gene query ids that return None (empty)
     
+<<<<<<< HEAD
 
     process_key = lambda k: k.replace(" ","_").lower() 
 
@@ -113,10 +145,80 @@ def load_orthology(data_folder):
             #print(json.dumps(record_list[:3], sort_keys=False, indent=4))
             id_record = {"_id": gene_id, "agr": {"orthologs": record_list}}
 
+=======
+    process_key = lambda k: k.replace(" ","_").lower() 
+
+    for gene1_id in unique_ids[:2]:
+        pairwise_check, gene_df=orthology_search(gene1_id, data_ortho) # get the orthology relationship
+        if pairwise_check==True:
+            
+            if ":" in gene1_id : gene1_id = gene1_id.split(":")[1]
+            
+            # query for the corresponding numeric id of the original id
+            gene=get_gene(gene1_id)#, gene_client)
+            
+            # check if gene id was not found
+            if not gene:
+
+                # check if gene in ZFIN database
+                if "Z" in gene1_id:
+                    try:
+                        temp_df=zfin_df.loc[zfin_df["ZFIN ID"] == gene1_id ]
+                        gene1_id=temp_df["NCBI Gene ID"].values[0]
+                    except:
+                        bad_queries.append(gene_id)
+
+                else:
+                    bad_queries.append(gene1_id) # add no matching id
+            else:
+                gene1_id = gene["_id"] # assign new id queried from mygene
+
+            # convert df into records
+            gene_df=gene_df.to_dict(orient='records')
+
+            record_list=[] # initialize gene1ID record list 
+            for rec in gene_df:
+                gene2_id=rec["Gene2ID"]
+                if ":" in gene2_id: gene2_id=gene2_id.split(":")[1]
+
+                gene2=get_gene(gene2_id)#, gene_client)
+
+                if not gene2:
+                    if "Z" in gene2_id:
+                        try:
+                            temp_df=zfin_df.loc[zfin_df["ZFIN ID"] == gene2_id ]
+                            gene2_id=temp_df["NCBI Gene ID"].values[0]
+                        except:
+                            if gene2_id not in bad_queries: bad_queries.append(gene2_id) 
+                            
+                    else:
+                        if gene2_id not in bad_queries: bad_queries.append(gene2_id) # add no matching id
+                else:
+                    gene2_id = gene2["_id"] # assign new id queried from mygene
+
+                # clean up data
+                rec = dict_convert(rec,keyfn=process_key)
+                # remove NaN values, not indexable
+                #rec = dict_sweep(rec,vals=[np.nan])
+
+                # setup document
+                doc=set_document(rec)
+                doc["geneid"]=gene2_id
+            
+                record_list.append(doc)
+            
+            #print(json.dumps(record_list[:3], sort_keys=False, indent=4))
+            id_record = {"_id": gene1_id, "agr": {"orthologs": record_list}}
+
+>>>>>>> 1b90fbc (adding updates from last few days)
             final_list.append(id_record)
         
         else:
             continue
     
+<<<<<<< HEAD
     # print(json.dumps(final_list[:5], sort_keys=False, indent=4))
     return final_list;
+=======
+    return final_list;
+>>>>>>> 1b90fbc (adding updates from last few days)
